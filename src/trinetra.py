273b1aa53069fb7e08f5cdb78300c9643a5640b6
Shree Hebbar <prasanteli@gmail.com>
2023-08-03 15:30:23 +0530

    json data loading fine

--- src/trinetra.py
+++ src/trinetra.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request 
-
+import json
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +9,20 @@ def homepage():
 @app.route("/about_us")
 def about_us():
     return render_template("about.html")
+
+@app.route("/scheme")
+def scheme():
+    with open('src\static\data.json', 'r') as f:
+        data = json.load(f)
+        return render_template("scheme.html",data=data)
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,  debug=True, load_dotenv="./.flaskenv")
